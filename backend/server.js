9b3e2775808aa097e7ed102bfbfc76addeb023a5
martin <martin.bartaa@gmail.com>
2024-11-30 15:54:42 +0200

    created routes

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,6 @@
-console.log("Hello");
+const express = require("express");
+const dotenv = require("dotenv").config();
+const port = process.env.PORT || 5001;
+const app = express();
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
