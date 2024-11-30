a22399ca0a2713b752478fa172b3c96350931b77
martin <martin.bartaa@gmail.com>
2024-11-30 16:38:03 +0200

    added error handler and stack error only for dev env

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,25 +1,33 @@
+const asyncHandler = require("express-async-handler");
+
 // @desc    Get goals
 // @route   GET /api/goals
-const getGoals = (req, res) => {
+const getGoals = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Get goals" });
-};
+});
 
 // @desc    Set goals
-// @route   POST /api/goals:id
-const setGoal = (req, res) => {
+// @route   POST /api/goals
+const setGoal = asyncHandler(async (req, res) => {
+  console.log(req.body);
+  if (!req.body.text) {
+    res.status(400);
+    throw new Error("Please add a text field");
+  }
+
   res.status(200).json({ message: "Set goal" });
-};
+});
 
 // @desc    Update goals
-// @route   PUT /api/goals
-const updateGoal = (req, res) => {
+// @route   PUT /api/goals:id
+const updateGoal = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Update goal ${req.params.id}` });
-};
+});
 
 // @desc    Delete goals
 // @route   DELETE /api/goals:id
-const deleteGoal = (req, res) => {
+const deleteGoal = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Delete goal ${req.params.id}` });
-};
+});
 
 module.exports = { getGoals, setGoal, updateGoal, deleteGoal };
