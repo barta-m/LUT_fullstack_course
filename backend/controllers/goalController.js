7d40547d687e80a5a945fcaed68398b3886174ea
martin <martin.bartaa@gmail.com>
2024-11-30 17:06:46 +0200

    dir structure modified - wrong structure

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,9 +1,11 @@
 const asyncHandler = require("express-async-handler");
+const Goal = require("../models/goalModel");
 
 // @desc    Get goals
 // @route   GET /api/goals
 const getGoals = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Get goals" });
+  const goals = await Goal.find();
+  res.status(200).json(goals);
 });
 
 // @desc    Set goals
