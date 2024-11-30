45f2b16c1220d977864578ab8d96787142285e4f
martin <martin.bartaa@gmail.com>
2024-11-30 17:17:58 +0200

    update and delete methods

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -27,13 +27,31 @@ const setGoal = asyncHandler(async (req, res) => {
 // @desc    Update goals
 // @route   PUT /api/goals:id
 const updateGoal = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Update goal ${req.params.id}` });
+  const goal = await Goal.findById(req.params.id);
+  if (!goal) {
+    res.status(400);
+    throw new Error("Goal not found");
+  }
+
+  const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+
+  res.status(200).json(updatedGoal);
 });
 
 // @desc    Delete goals
 // @route   DELETE /api/goals:id
 const deleteGoal = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Delete goal ${req.params.id}` });
+  const goal = await Goal.findById(req.params.id);
+  if (!goal) {
+    res.status(400);
+    throw new Error("Goal not found");
+  }
+
+  await goal.deleteOne();
+
+  res.status(200).json({ id: req.params.id });
 });
 
 module.exports = { getGoals, setGoal, updateGoal, deleteGoal };
