b59cd092e95ab786f48da609d11957951efc86f0
martin <martin.bartaa@gmail.com>
2024-11-30 16:17:08 +0200

    update controller and routes

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,5 +1,25 @@
+// @desc    Get goals
+// @route   GET /api/goals
 const getGoals = (req, res) => {
-  res.status(200).json({ message: "Get goalssss" });
+  res.status(200).json({ message: "Get goals" });
 };
 
-module.exports = { getGoals };
+// @desc    Set goals
+// @route   POST /api/goals:id
+const setGoals = (req, res) => {
+  res.status(200).json({ message: "Set goal" });
+};
+
+// @desc    Update goals
+// @route   PUT /api/goals
+const updateGoal = (req, res) => {
+  res.status(200).json({ message: `Update goal ${req.params.id}` });
+};
+
+// @desc    Delete goals
+// @route   DELETE /api/goals:id
+const deleteGoal = (req, res) => {
+  res.status(200).json({ message: `Delete goal ${req.params.id}` });
+};
+
+module.exports = { getGoals, setGoals, updateGoal, deleteGoal };
