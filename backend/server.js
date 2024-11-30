499ca5ba6b26558c3331aefccaa29adf9ca20d36
martin <martin.bartaa@gmail.com>
2024-11-30 16:56:43 +0200

    mongodb created and set up connection

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,11 @@ const express = require("express");
 const dotenv = require("dotenv").config();
 const port = process.env.PORT || 5001;
 const { errorHandler } = require("./middleware/errorMiddleware");
+const colors = require("colors");
+const connectDB = require("./config/db");
 const app = express();
+
+connectDB();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
